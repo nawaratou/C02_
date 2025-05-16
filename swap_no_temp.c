43fb2bbd6a8894d2672c0b775d0322b54419dea8
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-16 13:16:27 +0100

    corrige

--- swap_no_temp.c
+++ swap_no_temp.c
@@ -4,10 +4,11 @@ int main() {
     int a, b;
     scanf("%d %d", &a, &b);
     
+    printf("avant échange: a=%d b=%d\n", a, b);
     // Méthode d'échange par addition/soustraction
-    a = a + b;
-    b = a - b;
-    a = a - b;
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
     
     printf("Après échange: a = %d, b = %d\n", a, b);
     return 0;
