e5e1a60e99c39ceee9c73d8f59219a7b48e565d7
nawaratou <hamayacoubanawaratou@gmail.com>
2025-05-16 13:02:52 +0100

    script cinq

---
+++ swap_no_temp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    // Méthode d'échange par addition/soustraction
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    printf("Après échange: a = %d, b = %d\n", a, b);
+    return 0;
+}
